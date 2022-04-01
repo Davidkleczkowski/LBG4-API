42c41172842b392f45d5b355c59e6916ba48d865
Davidkleczkowski <davidkleczkowski@gmail.com>
2022-04-01 14:01:24 +0000

    updated file.sh once more

--- file.sh
+++ file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #CD ~/LBG4-APP
-#let VMPORT=5000
-#let CPORT=5000
+#
+#
 # test application
 
 npm install
@@ -10,7 +10,7 @@ npm test
 docker build -t davenodeimage:1 .
 #run image
 docker run --name davenodecontainer -d -p 5000:5000 --rm davenodeimage:1 .
-#let VMPORTADD = VMPORT+1
-#let  CPORTADD = CPORT+1
+#
+#
 # remove any untagged images
-#docker system prune -a -f
+docker system prune -a -f
